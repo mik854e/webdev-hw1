bb706dcb6395218d7583843d593ac0e36cda80f5
thebrianzeng <thebrianzeng@gmail.com>
2014-10-15 13:46:07 -0400

    added callback to params in customer facade

--- app/controllers/facades/customer_facade.js
+++ app/controllers/facades/customer_facade.js
@@ -4,19 +4,19 @@ var _ = require('lodash'),
 	mongoose = require('mongoose'),
 	crm_services = require('../CRM/crm_services.js');
 
-exports.getAgent = function(agentID) {
+exports.getAgent = function(agentID, callback) {
 	crm_services.getAgent(agentID, function(agent) {
 		callback(agent);
 	});
 };
 
-exports.getContactHistory = function(agentID, customerID) {
+exports.getContactHistory = function(agentID, customerID, callback) {
 	crm_services.getContactHistory(agentID, customerID, function(contacts) {
 		callback(contacts);
 	});
 };
 
-exports.getCustomer = function(customerID) {
+exports.getCustomer = function(customerID, callback) {
 	crm_services.getCustomer(customerID, function(customer) {
 		callback(customer);
 	});
